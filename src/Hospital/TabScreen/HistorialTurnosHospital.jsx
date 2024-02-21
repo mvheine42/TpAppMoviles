014756ab0f68af21e54f7a1540a5dd84c268a778
CamilaPlaza12 <camilaplaza12@uca.edu.ar>
2024-02-21 00:00:56 -0300

    HistorialTurnosHospital

--- src/Hospital/TabScreen/HistorialTurnosHospital.jsx
+++ src/Hospital/TabScreen/HistorialTurnosHospital.jsx
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Modal, TouchableOpacity, ScrollView } from 'react-native';
 import { Agenda } from 'react-native-calendars';
+import { format, parseISO } from 'date-fns';
 
 const API_URL = "http://localhost:3000";
 
 export const HistorialTurnosHospital = (props) => {
   const [historialTurnosData, setHistorialTurnosData] = useState([]);
   const [selectedDate, setSelectedDate] = useState(getFormattedCurrentDate);
+  const [selectedTurn, setSelectedTurn] = useState(null);
+  const [isModalVisible, setModalVisible] = useState(false);
+  const formattedDate = selectedTurn && selectedTurn.fecha ? format(parseISO(selectedTurn.fecha), 'dd/MM/yyyy') : '';
+  const formattedTime = selectedTurn ? selectedTurn.hora : '';
 
   useEffect(() => {
     // Obtener los turnos del hospital desde el servidor
@@ -29,6 +34,16 @@ export const HistorialTurnosHospital = (props) => {
     } catch (error) {
       console.error('Error fetching hospital turns:', error);
     }
+  };
+
+  const openModal = (turno) => {
+    setSelectedTurn(turno);
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setSelectedTurn(null);
+    setModalVisible(false);
   };
 
   const items = historialTurnosData.reduce((result, turno) => {
@@ -63,25 +78,67 @@ export const HistorialTurnosHospital = (props) => {
         pastScrollRange={3}
         futureScrollRange={3}
         renderItem={(item) => (
-          <View key={item.id} style={styles.turnBlock}>
-            <View style={styles.turnInfo}>
-              <Text style={styles.turnText}>Paciente: {item.donante.nombre} {item.donante.apellido}</Text>
-              <Text style={styles.turnText}>Nro Pedido: {item.pedidoHospital.id} - {item.pedidoHospital.tipoDonacion}</Text>
-              <Text style={styles.turnText}>Hora: {item.hora}</Text>
+          <TouchableOpacity onPress={() => openModal(item)}>
+            <View key={item.id} style={styles.turnBlock}>
+              <View style={styles.turnInfo}>
+                <Text style={styles.turnText}>Paciente: {item.donante.nombre} {item.donante.apellido}</Text>
+                <Text style={styles.turnText}>Donación: {item.pedidoHospital.tipoDonacion}</Text>
+                <Text style={styles.turnText}>Hora: {item.hora}</Text>
+              </View>
             </View>
-          </View>
+          </TouchableOpacity>
         )}
         renderEmptyData={renderEmptyData}
         onDayPress={(day) => setSelectedDate(day.dateString)}
         markedDates={markedDates}
         theme={{
-          selectedDotColor: '#BA181B', // Color personalizado para el día seleccionado
+          selectedDotColor: '#BA181B',
         }}
       />
+
+      {/* Modal para mostrar la información detallada del turno */}
+      <Modal 
+        visible={isModalVisible} 
+        style={styles.modal} 
+        animationType="slide" 
+        transparent={true}>
+        <View style={styles.modalContainer}>
+          <TouchableOpacity onPress={closeModal} style={styles.closeButton}>
+            <Text style={styles.closeButtonText}>X</Text>
+          </TouchableOpacity>
+            <ScrollView>
+              <Text style={styles.modalTitle}>Información del Turno</Text>
+              {/* Aquí puedes mostrar toda la información del donante */}
+              {selectedTurn && (
+                <View>
+                  <Text style={styles.modalInfo}>Pedido n°{selectedTurn.pedidoHospital.id} - {selectedTurn.pedidoHospital.tipoDonacion}</Text>
+                  <Text style={styles.modalInfo}>Fecha: {formattedDate} - {formattedTime}hs</Text>
+                  <View>
+                    <Text style={styles.modalInfoPacienteTitle}>Paciente</Text>
+
+                    <Text style={styles.modalInfoPaciente}>Donante: {selectedTurn.donante.nombre} {selectedTurn.donante.apellido}</Text>
+                    <Text style={styles.modalInfoPaciente}>DNI: {selectedTurn.donante.dni}</Text>
+                    <Text style={styles.modalInfoPaciente}>Género: {selectedTurn.donante.genero}</Text> 
+                    <Text style={styles.modalInfoPaciente}>Edad: {selectedTurn.donante.edad}</Text>
+                    <Text style={styles.modalInfoPaciente}>Peso: {selectedTurn.donante.peso}kg</Text>
+                    <Text style={styles.modalInfoPaciente}>Medicaciones: {selectedTurn.donante.medicaciones}</Text>
+                    <Text style={styles.modalInfoPaciente}>Embarazo: {selectedTurn.donante.embarazo}</Text>
+                    <Text style={styles.modalInfoPaciente}>Tipo de Sangre: {selectedTurn.donante.tipoSangre} {selectedTurn.donante.factorRH}</Text>
+                  </View>
+                  
+                </View>
+              )}
+              <TouchableOpacity onPress={closeModal} style={styles.modalCloseButton}>
+                <Text style={styles.modalCloseButtonText}>Cancelar Turno</Text>
+              </TouchableOpacity>
+            </ScrollView>
+          </View>
+      </Modal>
+
+      
     </View>
   );
 };
-
   
   const styles = StyleSheet.create({
     container: {
@@ -122,6 +179,64 @@ export const HistorialTurnosHospital = (props) => {
     },
     emptyDataText: {
       fontSize: 18,
+      color: '#A4161A',
+    },
+    modalContainer: {
+      flex: 1,
+      backgroundColor: 'white',
+      margin: 30,
+      marginTop: 110,
+      marginBottom: 110,
+      borderRadius: 10,
+      padding: 35,
+      color: 'black',
+      borderColor: '#BA181B',  // Color del borde
+      borderWidth: 2,
+    },
+    modalTitle: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      marginBottom: 10,
+      color: 'black',
+    },
+    modalInfo: {
+      fontSize: 15,
+      marginBottom: 5,
+      color: 'black',
+    },
+    modalInfoPacienteTitle: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      marginTop: 10,
+      marginBottom: 10,
+      color: 'black',
+    },
+    modalInfoPaciente: {
+      fontSize: 15,
+      marginBottom: 5,
+      color: 'black',
+    },
+    modalCloseButton: {
+      backgroundColor: '#BA181B',
+      padding: 10,
+      borderRadius: 5,
+      marginTop: 20,
+      alignItems: 'center',
+    },
+    modalCloseButtonText: {
+      color: 'white',
+      fontSize: 16,
+      fontWeight: 'bold',
+    },
+    closeButton: {
+      position: 'absolute',
+      top: 10,
+      right: 10,
+      padding: 10,
+      zIndex: 1,
+    },
+    closeButtonText: {
+      fontSize: 20,
       color: '#A4161A',
     },
   });
