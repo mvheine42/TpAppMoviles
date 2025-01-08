2f9ff6b1c06ccba99432df8c6b0ffbbf6d82d80f
Victoria Heine <127904639+mvheine42@users.noreply.github.com>
2025-01-07 21:57:56 -0300

    Notifs

--- App.tsx
+++ App.tsx
@@ -6,6 +6,18 @@ import store from "./src/redux/store";
 import { Provider } from "react-redux";
 import { connectScreen } from "./src/redux/helpers";
 
+import { requestMultiple, PERMISSIONS } from 'react-native-permissions';
+import messaging from '@react-native-firebase/messaging';
+
+messaging().getToken().then(t => console.log("Token:",t));
+messaging().setBackgroundMessageHandler( async remoteMessage => {
+  console.log("Mensaje en backround:", remoteMessage)
+})
+
+requestMultiple([PERMISSIONS.ANDROID.POST_NOTIFICATIONS]).then((statuses) => {
+  console.log("Notis perm", statuses[PERMISSIONS.ANDROID.POST_NOTIFICATIONS])
+})
+
 const StackNavigator = connectScreen(StackNavigatorScreen);
 
 const App = () => {
